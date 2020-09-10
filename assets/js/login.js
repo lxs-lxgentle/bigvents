9c7030729f33adcf8b112476c16a944b46713b4e
lxs <you@example.com>
2020-09-10 15:27:02 +0800

    login的部分

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,41 @@
 $(function () {
-  // link-login
   $("#link-login").on("click", function () {
     $(".login-box").hide();
     $(".reg-box").show();
   });
-  //link-reg-box
   $("#link-reg").on("click", function () {
-    $(".login-box").show();
     $(".reg-box").hide();
+    $(".login-box").show();
+  });
+
+  layui.form.verify({
+    password: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+    repassword: function (value) {
+      // if ($("#reg-psw").val() !== value) {
+      //   return "密码不一致";
+      // }
+      if ($("#reg-psd").val() !== value) {
+        return "密码不一致";
+      }
+    },
+  });
+
+  $("#form-reg").on("submit", function (e) {
+    e.preventDefault();
+    var username = $("#reg-username").val();
+    var password = $("#reg-psw").val();
+    var formdata = {
+      username: username,
+      password: password,
+    };
+  });
+  $.post("http://ajax.frontend.itheima.net/api/reguser", formdata, function (
+    res
+  ) {
+    if (res.status === 0) {
+      console.log(res, message);
+    } else {
+      console.log(res, message);
+    }
   });
 });
