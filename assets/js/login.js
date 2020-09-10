96c2c53150b4382d427a4d9cf15634a5186e2836
lxs <you@example.com>
2020-09-10 20:27:07 +0800

    第二天的代码

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,34 +8,66 @@ $(function () {
     $(".login-box").show();
   });
 
+  // layui.form.verify({
+  //   password: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+  //   repassword: function (value) {
+  //     // if ($("#reg-psw").val() !== value) {
+  //     //   return "密码不一致";
+  //     // }
+  //     if ($("#reg-psd").val() !== value) {
+  //       return "密码不一致";
+  //     }
+  //   },
+  // });
   layui.form.verify({
+    //LayUI的验证写法有二
+    // 1. 也支持下述数组的形式
+    // 2. 既支持上述函数式的方式
+    //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+
+    // login的form的rule
     password: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+
+    // reg的form的rule
     repassword: function (value) {
-      // if ($("#reg-psw").val() !== value) {
-      //   return "密码不一致";
-      // }
+      //value：表单的值
       if ($("#reg-psd").val() !== value) {
         return "密码不一致";
       }
     },
   });
 
-  $("#form-reg").on("submit", function (e) {
+  $("#btn-reg").on("submit", function (e) {
     e.preventDefault();
     var username = $("#reg-username").val();
-    var password = $("#reg-psw").val();
+    var password = $("#reg-psd").val();
     var formdata = {
       username: username,
       password: password,
     };
+    $.post("/api/reguser", formdata, function (res) {
+      // if (res.status === 0) {
+      //   $("#link-reg").click();
+      // }
+
+      // layui.layer.msg(res.message);
+      // // console.log("res", res.message);
+      layui.layer.msg(res.message);
+      res.status === 0 && $("#link-reg").click();
+    });
   });
-  $.post("http://ajax.frontend.itheima.net/api/reguser", formdata, function (
-    res
-  ) {
-    if (res.status === 0) {
-      console.log(res, message);
-    } else {
-      console.log(res, message);
-    }
+
+  $("#btn-login").on("submit", function (e) {
+    e.preventDefault();
+
+    var formdata = $(this).serialize();
+    $.post("/api/login", formdata, function (res) {
+      if (res.status === 0) {
+        window.location.href = "/index.html";
+        res.token.length !== 0 &&
+          window.localStorage.setItem("token", res.token);
+      }
+      layui.layer.msg(res.message);
+    });
   });
 });
